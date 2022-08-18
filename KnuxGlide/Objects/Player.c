3519161de145799276ac167b4c9355fe7680d033
TTTTTsd <49044793+TTTTTsd@users.noreply.github.com>
2022-08-17 23:49:39 -0400

    Add files via upload

--- KnuxGlide/Objects/Player.c
+++ KnuxGlide/Objects/Player.c
@@ -9,10 +9,12 @@ void Player_State_KnuxGlideDrop_Hook(void)
 
     if (self->onGround) {
             if (self->jumpPress & self->down) {
-                Player_Action_Spindash();
+                self->nextAirState = Player_State_Air;
+                Player_Action_Spindash(self);
                 self->skidding = 0;
             }
             if (self->jumpPress & !self->down) {
+                self->nextAirState = Player_State_Air;
                 Player_Action_Jump(self);
                 self->skidding = 0;
             }
